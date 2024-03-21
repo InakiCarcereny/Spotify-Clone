1404a7aa77a88a4679de42497078325458bd3688
inakicarcereny <inakicarce@hotmail.com.ar>
2024-03-21 18:27:54 -0300

    adding music player

---
+++ src/components/MusicPlayer.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+
+const PauseIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" >
+  <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"/>
+  <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"/>
+</svg>
+)
+
+const PlayIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
+  <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"/>
+  </svg>
+)
+
+export function MusicPlayer () {
+  const [isPlaying, setIsPlaying] = useState(false)
+  return (
+    <div className="text-white flex items-center justify-between mt-6">
+      <div> 
+        aca va la cancion que se esta reproduciendo...
+      </div>
+      <div className="flex flex-col items-center">
+        <button className="bg-white rounded-full px-1 py-1" onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? <PauseIcon /> : <PlayIcon />}
+        </button>
+      </div>
+      <div>
+        aca va el volumen de la cancion...
+      </div>
+    </div>
+  )
+}
